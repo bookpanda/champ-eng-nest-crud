8c12aaae3a196f20896d9c09879ab6f505236d62
Idhibhat Pankam <marginpankam@gmail.com>
2023-05-27 00:33:33 +0700

    feat: swagger descs

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -10,28 +10,41 @@ import {
 import { ListService } from './list.service';
 import { CreateListDto, UpdateListDto } from './dto';
 import { ParseIdPipe, ValidationPipe } from 'src/pipes';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('lists')
 @Controller('lists')
 export class ListController {
   constructor(private readonly listService: ListService) {}
 
+  @ApiOperation({
+    description: 'Create a new List from JSON.',
+  })
   @Post()
   create(@Body(ValidationPipe) createListDto: CreateListDto) {
     return this.listService.create(createListDto);
   }
 
+  @ApiOperation({
+    description: 'Get all Lists',
+  })
   @Get()
   findAll() {
     return this.listService.findAll();
   }
 
+  @ApiOperation({
+    description: 'Get a List with ID',
+  })
   @Get(':id')
   findOne(@Param('id', ParseIdPipe) id: number) {
     return this.listService.findOne(id);
   }
 
+  @ApiOperation({
+    description:
+      'Update a List of ID with JSON. Only changes the fields that are in the JSON.',
+  })
   @Put(':id')
   update(
     @Param('id', ParseIdPipe) id: number,
@@ -40,6 +53,9 @@ export class ListController {
     return this.listService.update(id, updateListDto);
   }
 
+  @ApiOperation({
+    description: 'Delete a List with ID, and its Tasks',
+  })
   @Delete(':id')
   remove(@Param('id', ParseIdPipe) id: number) {
     return this.listService.remove(id);
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -10,28 +10,41 @@ import {
 import { TaskService } from './task.service';
 import { CreateTaskDto, UpdateTaskDto } from './dto';
 import { ParseIdPipe, ValidationPipe } from 'src/pipes';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('tasks')
 @Controller('tasks')
 export class TaskController {
   constructor(private taskService: TaskService) {}
 
+  @ApiOperation({
+    description: 'Create a new Task from JSON.',
+  })
   @Post()
   create(@Body(ValidationPipe) createTaskDto: CreateTaskDto) {
     return this.taskService.create(createTaskDto);
   }
 
+  @ApiOperation({
+    description: 'Get all Tasks',
+  })
   @Get()
   findAll() {
     return this.taskService.findAll();
   }
 
+  @ApiOperation({
+    description: 'Get a Task with ID',
+  })
   @Get(':id')
   findOne(@Param('id', ParseIdPipe) id: number) {
     return this.taskService.findOne(id);
   }
 
+  @ApiOperation({
+    description:
+      'Update a Task of ID with JSON. Only changes the fields that are in the JSON.',
+  })
   @Put(':id')
   update(
     @Param('id', ParseIdPipe) id: number,
@@ -40,6 +53,9 @@ export class TaskController {
     return this.taskService.update(id, updateTaskDto);
   }
 
+  @ApiOperation({
+    description: 'Delete a Task with ID',
+  })
   @Delete(':id')
   remove(@Param('id', ParseIdPipe) id: number) {
     return this.taskService.remove(id);
